bb3516b50a6d6c9844572ebcc75e3042f5ebcad2
Matheus Rocha <matheusacrocha7@gmail.com>
2020-04-11 18:28:16 -0300

    Update Pares.exs

--- tarefa-01/Programas/Pares.exs
+++ tarefa-01/Programas/Pares.exs
@@ -2,7 +2,7 @@ inicio = IO.gets("Digite o numero inicial: ") |> String.trim |> String.to_intege
 {fim, _} = IO.gets("Digite o numero final: ") |> Integer.parse
 lista = Enum.to_list(inicio..fim)
 IO.puts "Lista criada: "
-IO.inspect lista
+IO.inspect(lista, limit: :infinity)
 pares = for n <- lista, rem(n, 2) == 0, do: n
 IO.puts "Lista contendo somente numeros pares: "
-IO.inspect pares
+IO.inspect(pares, limit: :infinity)
